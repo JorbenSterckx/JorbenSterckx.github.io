c9c43b6b1596bf42a63755798357c7b92b004834
Jorben Sterckx <113974674+JorbenSterckx@users.noreply.github.com>
2024-02-19 13:21:30 +0000

    Third Commit

--- script.js
+++ script.js
@@ -6,20 +6,36 @@ document.getElementById('toggleButton').addEventListener('click', function() {
     // Toggle expanded class on sidebar
     sidebar.classList.toggle('expanded');
   
-    if (sidebar.classList.contains('expanded')) {
-      // Expand sidebar
-      sidebar.style.width = '150px';
-      content.style.marginLeft = '40px';
-      // Change icon to "fa-angles-left" when sidebar is expanded
-      icon.classList.remove('fa-angles-right');
-      icon.classList.add('fa-angles-left');
-    } else {
-      // Collapse sidebar
-      sidebar.style.width = '40px';
-      content.style.marginLeft = '0px';
-      // Change icon to "fa-angles-right" when sidebar is collapsed
-      icon.classList.remove('fa-angles-left');
-      icon.classList.add('fa-angles-right');
+    if (window.innerWidth > 768) { // Check if screen size is larger than 768px
+      if (sidebar.classList.contains('expanded')) {
+        // Expand sidebar
+        sidebar.style.width = '150px';
+        content.style.marginLeft = '40px';
+        // Change icon to "fa-angles-left" when sidebar is expanded
+        icon.classList.remove('fa-angles-right');
+        icon.classList.add('fa-angles-left');
+      } else {
+        // Collapse sidebar
+        sidebar.style.width = '40px';
+        content.style.marginLeft = '0px';
+        // Change icon to "fa-angles-right" when sidebar is collapsed
+        icon.classList.remove('fa-angles-left');
+        icon.classList.add('fa-angles-right');
+      }
+    }
+
+    if (window.innerWidth < 768) { // Check if screen size is smaller than 768px {
+      if (sidebar.classList.contains('expanded')) {
+        sidebar.style.height = '100px'; // Set the height as needed
+        sidebar.style.width = '100%';
+        icon.classList.remove('fa-angles-right');
+        icon.classList.add('fa-angles-up');
+      } else {
+        sidebar.style.height = '40px'; // Set the height as needed
+        sidebar.style.width = '100%';
+        icon.classList.remove('fa-angles-up');
+        icon.classList.add('fa-angles-down');
+      }
     }
   });
   
